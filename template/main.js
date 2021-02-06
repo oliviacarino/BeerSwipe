14b2f12105bde256a2748df2fe9ca831b5c4d209
Olivia <51516158+oliviacarino@users.noreply.github.com>
2021-02-06 15:27:40 -0500

    Added about and swipe game sections

--- template/main.js
+++ template/main.js
@@ -1,10 +1,18 @@
 // Liquid Preloader
-(function($){
-  'use strict';
-    $(window).on('load', function () {
-        if ($(".loader").length > 0)
-        {
-            $(".loader").fadeOut("slow");
-        }
-    });
-})(jQuery)
+// (function($){
+//   'use strict';
+//     $(window).on('load', function () {
+//         if ($(".loader").length > 0)
+//         {
+//             $(".loader").fadeOut("slow");
+//         }
+//     });
+// })(jQuery)
+// var loader = document.getElementByClass("loader");
+// window.onload = function () {
+//     loader.style.display = 'none';
+// }
+// $.wait = function( callback, seconds){
+//    return window.setTimeout( callback, seconds * 1000 );
+// }
+// $.wait( function(){ $("#loader").slideUp() }, 5);
